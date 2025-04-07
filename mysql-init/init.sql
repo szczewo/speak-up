8c163ed32c9e16a4a9558c008ac0a7c5a4cbe385
AAnna Strzewiczek <aniastrzewiczek@gmail.com>
2025-04-07 16:41:29 +0000

    Fix: add .env copy and mysql-init/init.sql copy steps, remove MYSQL init.sql from repo

--- mysql-init/init.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS development;
-CREATE USER IF NOT EXISTS 'development'@'%' IDENTIFIED BY 'development';
-GRANT ALL PRIVILEGES ON development.* TO 'development'@'%';
-FLUSH PRIVILEGES;
