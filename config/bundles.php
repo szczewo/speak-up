7531ede469f90d4f3b87e2d36182990f53de6559
Anna Strzewiczek <aniastrzewiczek@gmail.com>
2025-08-31 19:14:32 +0200

    Fix: Remove reset password bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
