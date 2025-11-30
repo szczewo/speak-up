f177bbb044cde26d39c89b9dd58c519c73c563e0
Anna Strzewiczek <aniastrzewiczek@gmail.com>
2025-11-30 18:58:01 +0100

    Feat: Add ResetPasswordRequestedEvent and listener for sending reset password emails

---
+++ src/Event/ResetPasswordRequestedEvent.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Event;
+
+use App\Entity\ResetPasswordRequest;
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * Event triggered when a password reset is requested.
+ */
+class ResetPasswordRequestedEvent extends Event
+{
+    public const NAME = 'user.reset_password_requested'
+;
+
+    public function __construct(
+        private readonly ResetPasswordRequest $resetPasswordRequest
+    ) {}
+
+  public function getResetPasswordRequest(): ResetPasswordRequest
+  {
+      return $this->resetPasswordRequest;
+  }
+
+}
