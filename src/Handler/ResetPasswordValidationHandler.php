c7e5e3f766ae1cb625f96dd01dca24af9aef3327
Anna Strzewiczek <aniastrzewiczek@gmail.com>
2025-12-06 19:02:13 +0100

    Refactor: Use ResetPasswordRequestRepository and fix password verification logic

--- src/Handler/ResetPasswordValidationHandler.php
+++ src/Handler/ResetPasswordValidationHandler.php
@@ -3,27 +3,24 @@
 namespace App\Handler;
 
 use App\Entity\ResetPasswordRequest;
-use Doctrine\ORM\EntityManagerInterface;
-use Exception;
+use App\Repository\ResetPasswordRequestRepository;
 use InvalidArgumentException;
 
-
 /**
- * Handles reset password token validation
+ * Handles reset password token validation.
  */
 class ResetPasswordValidationHandler
 {
     public function __construct(
-        private EntityManagerInterface $em,
+        private ResetPasswordRequestRepository $repository,
     ) {}
 
     /**
-     * @throws Exception
+     * @throws InvalidArgumentException
      */
-    public function validate( string $selector, string $token) : ResetPasswordRequest
+    public function validate(string $selector, string $token): ResetPasswordRequest
     {
-        $resetPasswordRequest = $this->em->getRepository(ResetPasswordRequest::class)
-            ->findOneBy(['selector' => $selector]);
+        $resetPasswordRequest = $this->repository->findOneBySelector($selector);
 
         if (!$resetPasswordRequest) {
             throw new InvalidArgumentException('Invalid reset password token.');
@@ -33,11 +30,10 @@ class ResetPasswordValidationHandler
             throw new InvalidArgumentException('Reset password token has expired.');
         }
 
-        if (password_verify($token, $resetPasswordRequest->getHashedToken())) {
+        if (!password_verify($token, $resetPasswordRequest->getHashedToken())) {
             throw new InvalidArgumentException('Invalid reset password token.');
         }
 
         return $resetPasswordRequest;
     }
-
 }
